1f3e67ea5af6985c94da360d5205af4cacb8c5c1
Sergei <serg.minasyan98@gmail.com>
2021-10-06 22:04:05 +0400

    homework1

--- src/FigurePainter.java
+++ src/FigurePainter.java
@@ -1,2 +1,11 @@
 public class FigurePainter {
+
+    public static void main(String[] args) {
+        String astx = "";
+        for (int i=0;  i<4; i++){
+            astx = astx + "*";
+            System.out.println(astx);
+
+        }
+    }
 }
