a81fd62beb8e9e1251e91a002a63775e68d1733b
Sergei <serg.minasyan98@gmail.com>
2021-10-09 23:57:33 +0400

    homework1

---
+++ src/lesson5/IncrementExample.java
@@ -0,0 +1,10 @@
+package lesson5;
+
+public class IncrementExample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x++;
+        System.out.println("y = " + y);
+        System.out.println("x = " + x);
+    }
+}
