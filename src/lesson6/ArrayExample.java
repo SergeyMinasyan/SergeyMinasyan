c77aabf5946c3ffe562204ac3021bd92028f714a
Sergei <serg.minasyan98@gmail.com>
2021-10-12 13:23:51 +0400

    homework1

---
+++ src/lesson6/ArrayExample.java
@@ -0,0 +1,26 @@
+package lesson6;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 5, 5};
+        char[] name = new char[5];
+        name[0] = 'P';
+        name[1] = 'o';
+        name[2] = 'x';
+        name[3] = 'o';
+        name[4] = 's';
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+
+        }
+        for (int i = 0; i < name.length; i++) {
+            char c = name[i];
+            if(c== 'o');
+
+            
+        }
+    }
+    
+}
+ 
