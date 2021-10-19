b8698c4005a81324f8da6712453ce04232de35c7
Sergei <serg.minasyan98@gmail.com>
2021-10-19 13:41:30 +0400

    homework1

--- src/homework/Figurepainterr.java
+++ src/homework/Figurepainterr.java
@@ -2,16 +2,42 @@ package homework;
 
 public class Figurepainterr {
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
             for (int j = 4; j > i; j--) {
                 System.out.print("  ");
-
             }
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4 ; j >=i ; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j >i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+            for (int j = 0; j <=i; j++) {
+                System.out.print("  ");
+            }
+        }
+        }
     }
-}
+
 
