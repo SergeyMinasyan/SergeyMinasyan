c5683bece09922941901c9cc90cb5b8df7f8eec2
Sergei <serg.minasyan98@gmail.com>
2021-11-03 14:08:38 +0400

    homework1

---
+++ src/lesson11/Test.java
@@ -0,0 +1,19 @@
+package lesson11;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.err.println("Please input name");
+        String name = scanner.nextLine();
+        System.err.println("Please input age");
+        int age = scanner.nextInt();
+        System.err.println("Please input number 1");
+        int number1 = scanner.nextInt();
+        System.err.println("Please input number 2");
+        int number2 = scanner.nextInt();
+
+}
+
+}
