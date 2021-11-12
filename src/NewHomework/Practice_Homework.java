fe239183ceb74088124ab3e5c1acf35eee38b55f
Sergei <serg.minasyan98@gmail.com>
2021-11-12 11:02:19 +0400

    homework1

---
+++ src/NewHomework/Practice_Homework.java
@@ -0,0 +1,15 @@
+package NewHomework;
+
+public class Practice_Homework {
+    public static void main(String[] args) {
+        PracticeHomework practiceHomework =new PracticeHomework();
+        System.out.println(practiceHomework.convert(2));
+        System.err.println(practiceHomework.nextNumber(5));
+        System.out.println(practiceHomework.reverseBool(false));
+        System.err.println(practiceHomework.lessThanOrEqualToZero(-1));
+        System.out.println(practiceHomework.isSameNum(20, 20));
+        System.err.println(practiceHomework.calcAge(2));
+        int []array1 = {1,2,2,3};
+        int[] array2 = {1,1,1,1,1,1,1,1};
+    }
+}
