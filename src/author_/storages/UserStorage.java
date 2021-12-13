35fe21e37e903bbb17d48a066f48113022fd856b
Minasyan <Sergeijan1234>
2021-12-13 14:27:26 +0400

    homework1

---
+++ src/author_/storages/UserStorage.java
@@ -0,0 +1,46 @@
+package author_.storages;
+
+import author_.exception.UserNotFoundException;
+import author_.model.User;
+
+public class UserStorage {
+
+    private User[] users = new User[16];
+    private int size;
+
+    public void add(User user) {
+        if (users.length == size) {
+            extend();
+        }
+        users[size++] = user;
+    }
+
+    private void extend() {
+        User[] tmp = new User[users.length + 10];
+        System.arraycopy(users, 0, tmp, 0, users.length);
+        users = tmp;
+    }
+
+
+    public User getByEmail(String email) throws UserNotFoundException {
+        for (int i = 0; i < size; i++) {
+            if (users[i].getEmail().equals(email)) {
+                return users[i];
+            }
+        }
+throw new UserNotFoundException("User does not exists. email "+ email);
+    }
+
+    //TODO add implementation later.
+    public User getByEmailAndPassword(String email, String password) {
+        return null;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(users[i]);
+        }
+    }
+
+}
