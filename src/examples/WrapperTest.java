8c8b7c2f908ae2471927db87f66c6dc8fd8478d5
Minasyan <Sergeijan1234>
2021-12-16 20:31:35 +0400

    homework1

---
+++ src/examples/WrapperTest.java
@@ -0,0 +1,10 @@
+package examples;
+
+public class WrapperTest {
+    public static void main(String[] args) {
+        Integer a = 400;
+        Integer b = 400;
+        System.out.println(a.equals(b));
+        System.out.println(a==b);
+    }
+}
