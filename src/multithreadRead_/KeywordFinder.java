c8be703b018232cacba4ea691ddca992d99081a0
Minasyan <Sergeijan1234>
2022-01-14 20:51:49 +0400

    homework1

---
+++ src/multithreadRead_/KeywordFinder.java
@@ -0,0 +1,25 @@
+package multithreadRead_;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+class KeywordFinder implements Runnable {
+    public static AtomicInteger countOfKeyword = new AtomicInteger();
+    final String keyword =";";
+    Stream <String> lines;
+    public KeywordFinder(Stream lines) {
+        this.lines=lines;
+    }
+
+    public static AtomicInteger getCount(){
+        return countOfKeyword;
+    }
+    @Override
+    public void run() {
+        this.lines.forEach((str)->{
+            if (str.contains(keyword)) {
+                countOfKeyword.incrementAndGet();
+            }
+        });
+    }
+}
